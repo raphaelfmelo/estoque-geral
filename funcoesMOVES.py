882ac9951a7c52eb36417b21ce49d745637f75d5
snBuddha <snzraphael@gmail.com>
2026-08-17 13:41:42 -0300

    feat: registra histórico de movimentações

--- funcoesMOVES.py
+++ funcoesMOVES.py
@@ -1,5 +1,7 @@
 from funcoesCRUD import inicializar
 
+
+#Função para registrar a entrada de um produto; Inserindo produto e atualizando o estoque
 def registrar_entrada(id_produto, quantidade):
     conexao, cursor = inicializar()
 
@@ -9,9 +11,16 @@ def registrar_entrada(id_produto, quantidade):
         WHERE id = ?
     """, (quantidade, id_produto))
 
+    cursor.execute("""
+        INSERT INTO movimentacoes (produto_id, tipo, quantidade)
+        VALUES (?, ?, ?)
+    """, (id_produto, 'entrada', quantidade))
+
     conexao.commit()
     conexao.close()
 
+
+#Função para registrar a saída de um produto identificando o produto, estoque e analisando se é possível antes de atualizar
 def registrar_saida(id_produto, quantidade):
     conexao, cursor = inicializar()
 
@@ -36,5 +45,26 @@ def registrar_saida(id_produto, quantidade):
         WHERE id = ?
     """, (quantidade, id_produto))
 
+    cursor.execute("""
+            INSERT INTO movimentacoes (produto_id, tipo, quantidade)
+            VALUES (?, ?, ?)
+        """, (id_produto, 'saida', quantidade))
+
     conexao.commit()
     conexao.close()
+
+
+# Função para listar movimentações nomeando produtos
+def listar_movimentacoes():
+    conexao, cursor = inicializar()
+
+    cursor.execute("""
+        SELECT produtos.nome, movimentacoes.tipo, movimentacoes.quantidade, movimentacoes.data
+        FROM movimentacoes
+        JOIN produtos
+        ON movimentacoes.produto_id = produtos.id
+    """,)
+
+    movimentacoes = cursor.fetchall()
+    conexao.close()
+    return movimentacoes
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from funcoesCRUD import listar_produtos, cadastrar_produto, atualizar_produto, excluir_produto
-from funcoesMOVES import registrar_entrada, registrar_saida
+from funcoesCRUD import inicializar, listar_produtos, cadastrar_produto, atualizar_produto, excluir_produto
 
+from funcoesMOVES import registrar_entrada, registrar_saida, listar_movimentacoes
+
+conexao, cursor = inicializar()
+
+'''
 produtos = listar_produtos()
 for produto in produtos: print(produto)
 print()
+'''
 
 #Teste da Função Cadastrar
 '''
@@ -40,9 +45,24 @@ for produto in produtos: print(produto)
 '''
 
 #Teste das Funções Registrar (Entrada e Saída)
-    #registrar_saida (1, 5)
-    #registrar_saida(1, 1000)
-    #registrar_saida(1, 3)
-    #registrar_entrada(1, 5)
-    #produtos = listar_produtos()
-    #for produto in produtos: print(produto)
+#registrar_saida (1, 5)
+#registrar_saida(1, 1000)
+#registrar_saida(1, 3)
+#registrar_entrada(1, 5)
+#produtos = listar_produtos()
+#for produto in produtos: print(produto)
+#registrar_entrada(1, 3)
+
+cursor.execute("""
+    SELECT * FROM movimentacoes
+""")
+
+movimentacoes = cursor.fetchall()
+for movimentacao in movimentacoes:
+    print(movimentacao)
+
+print()
+
+movimentacoees = listar_movimentacoes()
+for move in movimentacoees:
+    print(move)
